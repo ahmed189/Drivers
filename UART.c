144b570a2fd81ba6e888154b82ac83911d88dfb7
ahmed189 <ahmedhakem189@gmail.com>
2019-08-02 19:30:34 +0200

    first commit

---
+++ UART.c
@@ -0,0 +1,125 @@
+#include "UART.h"
+
+
+
+int UART_Init(void)
+{
+    //Disables global interrupts and begins a critical section
+    void OSLock();
+    /*
+        configuration of registers:
+        - Configuration PINS to use as TX and RX not GPIOS if valid
+        - Enable Clock
+        - Sample mode
+        - Number of data bits
+        - specify number of stop bits
+        - specify parity check configuration
+        - Baudrate
+        - Enable uart
+        - Enable Transmitter and Receiver
+        - Enable interrupts we need
+    */
+        Uart.TX_INT = 1;
+        Uart.RX_INT = 1;
+
+    /*initialize interrupts handlers*/
+    simpl_install_handler(POSITION_HANDLER_TX_READY,&TX_Ready);
+    simpl_install_handler(POSITION_HANDLER_RX_READY,&RX_Ready);
+
+    Uart_Buffer_Init();
+
+    /*Create semaphore*/
+    Sem = OSCreateSemaphore(1);
+
+    //Enable global interrupts and Finish a critical section
+    void OSUnlock();
+    //for debugging
+    return 1;
+}
+
+void TX_Ready(void)
+{
+    //Atomically decrease the semaphore value by 1. If the value is negative, suspends the calling thread.
+    OSSemaphoreWait(sem);
+
+    if(UART_write(&buffer , LENGTH_OF_DATA_BYTES) == 0)
+        /*error handling mode*/
+    else;
+
+    //
+    OSSemaphoreSignal(sem);
+}
+void RX_Ready(void)
+{
+    //Atomically decrease the semaphore value by 1. If the value is negative, suspends the calling thread.
+    OSSemaphoreWait(sem);
+
+    if(UART_read(&buffer , LENGTH_OF_DATA_BYTES) == 0)
+        /*error handling mode*/
+    else;
+
+    //Atomically increments the semaphore value by 1. If the value is non positive, wakes up a suspended thread
+    OSSemaphoreSignal(sem);
+}
+
+int Uart_Buffer_Init(void)
+{
+    Uart_Buffer.Uart_Write_From_Buffer = 0;
+    Uart_Buffer.Uart_Write_To_Buffer   = 0;
+}
+
+int UART_read(uint8_t *buffer, int len)
+{
+    static uint8_t index_R = 0;
+
+    //check size of read buffer
+    if(len < MAX_BUFFER_SIZE)
+    {
+        index_R++;
+
+        //check size of the data read
+        if(index_R > len)
+        {
+            Uart_Buffer.Uart_Write_To_Buffer = 0;
+            index_R = 1;
+        }
+        else;
+        *(buffer + Uart_Buffer.Uart_Write_To_Buffer) = Uart.RX;
+        Uart_Buffer.Uart_Write_To_Buffer++;
+    }
+    else
+    {
+        return 0;
+    }
+    return 1;
+}
+
+
+
+
+
+int UART_write(uint8_t *buffer, int len)
+{
+    static uint8_t index_W = 0;
+    //check size of read buffer
+    if(len < MAX_BUFFER_SIZE)
+    {
+        index_W++;
+
+        //check size of the data read
+        if(index_W > len)
+        {
+            Uart_Buffer.Uart_Write_From_Buffer = 0;
+            index_W = 1;
+        }
+        else;
+        Uart.TX = *(buffer + Uart_Buffer.Uart_Write_From_Buffer);
+        Uart_Buffer.Uart_Write_From_Buffer++;
+    }
+    else
+    {
+        return 0;
+    }
+    return 1;
+
+}
---
+++ UART.h
@@ -0,0 +1,79 @@
+#ifndef UART_H_INCLUDED
+#define UART_H_INCLUDED
+
+/*fictitious Library for fictitious ARM*/
+#include "simplARM.h"
+
+#define MAX_BUFFER_SIZE             255
+
+/*Range of length from 1 -> 255*/
+#define LENGTH_OF_DATA_BYTES        1
+
+#define POSITION_HANDLER_TX_READY   (fictitious number)
+#define POSITION_HANDLER_RX_READY   (fictitious number)
+
+
+
+/*creating global buffer*/
+uint8_t buffer[MAX_BUFFER_SIZE]={0};
+
+/*semaphore*/
+     Semaphore Sem;
+
+/*status of UART*/
+typedef enum UART_Status
+{
+    UART_IDLE,
+    UART_WAIT_RESPONSE,
+    UART_WAIT_ASKING
+}Uart_Status;
+
+/*StATUS OF INTERRUPT*/
+typedef enum INTERRUPT_STATUS
+{
+    IRQ_NONE,
+    IRQ_RX_READY,
+    IRQ_TX_READY
+}Interrupt_Status;
+
+/*Data length from buffer or from buffer*/
+typedef struct UART_Write_Buffer
+{
+    uint8_t Uart_Write_From_Buffer;
+    uint8_t Uart_Write_To_Buffer;
+}Uart_Buffer;
+
+/*UART Registers 32 bits*/
+typedef struct UART{
+    uint32_t RX;
+    uint32_t TX;
+    uint32_t RX_READY;
+    uint32_t TX_READY;
+    uint32_t TX_INT;
+    uint32_t RX_INT;
+    uint32_t INTPOS;
+}Uart;
+
+
+
+
+/*standard data types*/
+typedef unsigned int uint32_t;
+typedef unsigned char uint8_t;
+
+
+
+
+
+
+/*API would be used*/
+int UART_Init(void);
+int UART_read(uint8_t *, int );
+int UART_write(uint8_t *, int);
+
+int Uart_Buffer_Init(void);
+
+/*interrupts*/
+void TX_Ready(void);
+void RX_Ready(void);
+#endif // UART_H_INCLUDED
